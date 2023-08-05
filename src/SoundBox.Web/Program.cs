c8000196a8393c776d4e0a66e7abb46ae9c974c0
Hakim Ghods <ghods.hakim@hotmail.com>
2023-08-05 18:48:33 +0330

    Minimal API

--- src/SoundBox.Web/Program.cs
+++ src/SoundBox.Web/Program.cs
@@ -1,5 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using SoundBox.Infrastructure.Persistence.DataModel;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ChinookDbContext>(options =>
+{
+    options.UseSqlite("Data Source=chinook.sqlite");
+});
 var app = builder.Build();
+
+app.MapGet("/api/test", (ChinookDbContext db) =>
+{
+    return Results.Ok(db.Employees.Select(e => new { e.FirstName,e.LastName }));
+});
 
 app.MapGet("/", () => "Hello World!"); // Minimal API
 
