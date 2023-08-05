77da8b7732095ec9570c705bac935f37b9c9730f
Hakim Ghods <ghods.hakim@hotmail.com>
2023-08-05 19:48:45 +0330

    Employee

---
+++ src/SoundBox.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoundBox.Infrastructure.Persistence.DataModel;
+
+namespace SoundBox.Web.Controllers;
+
+public class EmployeeController:ControllerBase
+{
+    private readonly ChinookDbContext _db;
+
+    public EmployeeController(ChinookDbContext db)
+    {
+        _db = db;
+    }
+
+    [Route("api/employees")]
+    public ActionResult GetEmployeeInfo()
+    {
+        return Ok(_db.Employees.Select(e=>new {e.FirstName, e.LastName}).ToList());
+    }
+}
--- src/SoundBox.Web/Program.cs
+++ src/SoundBox.Web/Program.cs
@@ -3,17 +3,13 @@ using SoundBox.Infrastructure.Persistence.DataModel;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
 builder.Services.AddDbContext<ChinookDbContext>(options =>
 {
     options.UseSqlite("Data Source=chinook.sqlite");
 });
 var app = builder.Build();
 
-app.MapGet("/api/test", (ChinookDbContext db) =>
-{
-    return Results.Ok(db.Employees.Select(e => new { e.FirstName,e.LastName }));
-});
-
-app.MapGet("/", () => "Hello World!"); // Minimal API
-
+app.MapControllers();
 app.Run();
